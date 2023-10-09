26dd4c4f94e6f1f1e47b3219ca0e31d03a7df28c
João Paulo Penney <137862398+joaopaulopenney@users.noreply.github.com>
2023-10-09 07:12:05 -0300

    Estudo MySQL-W3Schools

--- estudos.sql
+++ estudos.sql
@@ -174,3 +174,125 @@
 -- É possível excluir todas as linhas de uma tabela sem excluir a tabela. Isso significa que a estrutura da tabela, os atributos e os índices estarão intactos
 
 -- DELETE FROM Customers;
+
+
+
+-- MySQL LIMIT
+-- SELECT column_name(s)
+-- FROM table_name
+-- WHERE condition
+-- LIMIT number;
+
+-- SELECT * FROM Customers
+-- LIMIT 3;
+-- seleciona os três primeiros registros da tabela "Clientes"
+
+-- SELECT * FROM Customers
+-- LIMIT 3 OFFSET 3;
+-- "retornar apenas 3 registros, iniciar no registro 4 (OFFSET 3)"
+
+-- SELECT * FROM Customers
+-- WHERE Country='Germany'
+-- LIMIT 3;
+-- seleciona os três primeiros registros da tabela “Clientes”, onde o país é “Alemanha”
+
+
+
+-- MySQL MIN and MAX
+-- SELECT MIN(column_name)
+-- FROM table_name
+-- WHERE condition;
+-- A MIN()função retorna o menor valor da coluna selecionada
+
+-- SELECT MAX(column_name)
+-- FROM table_name
+-- WHERE condition;
+-- A MAX()função retorna o maior valor da coluna selecionada
+
+-- SELECT MIN(Price) AS SmallestPrice
+-- FROM Products;
+-- encontra o preço do produto mais barato
+
+-- SELECT MAX(Price) AS LargestPrice
+-- FROM Products;
+-- encontra o preço do produto mais caro
+
+
+
+-- MySQL COUNT, AVG, SUM
+-- SELECT COUNT(column_name)
+-- FROM table_name
+-- WHERE condition;
+-- retorna o número de linhas que corresponde a um critério especificado
+
+-- SELECT AVG(column_name)
+-- FROM table_name
+-- WHERE condition;
+-- retorna o valor médio de uma coluna numérica
+
+-- SELECT SUM(column_name)
+-- FROM table_name
+-- WHERE condition;
+-- retorna a soma total de uma coluna numérica
+
+-- SELECT COUNT(ProductID)
+-- FROM Products;
+-- encontra o número de produtos
+-- Nota: Valores NULL não são contados
+
+-- SELECT AVG(Price)
+-- FROM Products;
+-- encontra o preço médio de todos os produtos
+-- Nota: Valores NULL são ignorados
+
+-- SELECT SUM(Quantity)
+-- FROM OrderDetails;
+-- encontra a soma dos campos "Quantidade" na tabela "OrderDetails"
+-- Nota: Valores NULL são ignorados
+
+
+
+-- MySQL LIKE
+-- O sinal de porcentagem (%) representa zero, um ou vários caracteres
+-- O sinal de sublinhado (_) representa um único caractere
+
+-- SELECT column1, column2, ...
+-- FROM table_name
+-- WHERE columnN LIKE pattern;
+-- Você também pode combinar qualquer número de condições usando operadores AND ou OR
+
+-- WHERE CustomerName LIKE 'a%'	Finds any values that start with "a"
+-- WHERE CustomerName LIKE '%a' Finds any values that end with "a"
+-- WHERE CustomerName LIKE '%or%' Finds any values that have "or" in any position
+-- WHERE CustomerName LIKE '_r%' Finds any values that have "r" in the second position
+-- WHERE CustomerName LIKE 'a_%' Finds any values that start with "a" and are at least 2 characters in length
+-- WHERE CustomerName LIKE 'a__%' Finds any values that start with "a" and are at least 3 characters in length
+-- WHERE ContactName LIKE 'a%o'	Finds any values that start with "a" and ends with "o"
+
+-- SELECT * FROM Customers
+-- WHERE CustomerName LIKE 'a%';
+-- seleciona todos os clientes com um CustomerName começando com "a"
+
+-- SELECT * FROM Customers
+-- WHERE CustomerName LIKE '%a';
+-- seleciona todos os clientes com CustomerName terminando com "a"
+
+-- SELECT * FROM Customers
+-- WHERE CustomerName LIKE '%or%';
+-- seleciona todos os clientes com CustomerName que possuem "ou" em qualquer posição
+
+-- SELECT * FROM Customers
+-- WHERE CustomerName LIKE '_r%';
+-- seleciona todos os clientes com um CustomerName que tenha “r” na segunda posição
+
+-- SELECT * FROM Customers
+-- WHERE CustomerName LIKE 'a__%';
+-- seleciona todos os clientes com um CustomerName que começa com "a" e tem pelo menos 3 caracteres de comprimento
+
+-- SELECT * FROM Customers
+-- WHERE ContactName LIKE 'a%o';
+-- seleciona todos os clientes com um ContactName que começa com "a" e termina com "o"
+
+-- SELECT * FROM Customers
+-- WHERE CustomerName NOT LIKE 'a%';
+-- seleciona todos os clientes com um CustomerName que NÃO começa com "a"
